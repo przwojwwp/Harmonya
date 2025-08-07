f5507d7af083788d34468bef7719ca4b4497735f
wojciechprzyluski <przyluskiwojciech@gmail.com>
2025-08-07 13:31:13 +0200

    feat: adjust testimonial slider

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -1,59 +1,125 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./Testimonials.module.scss";
 import cn from "classnames";
 
 const testimonials = [
   {
     id: "anna-kowalska",
-    text: `
-      Współpraca z Harmonya Agency całkowicie odmieniła mój biznes.
-      Ich holistyczne podejście do marketingu pozwoliło mi dotrzeć do nowej grupy klientów,
-      a moje social media w końcu wyglądają profesjonalnie i spójnie.`,
+    text: `Współpraca z Harmonya Agency całkowicie odmieniła mój biznes.
+    Ich holistyczne podejście do marketingu pozwoliło mi dotrzeć do nowej grupy klientów,
+    a moje social media w końcu wyglądają profesjonalnie i spójnie.`,
     author: "Anna Kowalska",
     role: `Studio Jogi "Spokój"`,
   },
   {
     id: "marek-nowak",
-    text: `
-      Najbardziej cenię ich zaangażowanie i zrozumienie specyfiki branży wellness.
-      Czuję, że mój marketing jest w dobrych rękach, a ja mogę skupić się na pracy z klientami,
-      zamiast martwić się o Instagram czy newslettery.`,
+    text: `Najbardziej cenię ich zaangażowanie i zrozumienie specyfiki branży wellness.
+    Czuję, że mój marketing jest w dobrych rękach, a ja mogę skupić się na pracy z klientami,
+    zamiast martwić się o Instagram czy newslettery.`,
     author: "Marek Nowak",
     role: `Centrum Holistyczne "Harmonia"`,
   },
   {
     id: "karolina-wisniewska",
-    text: `
-      Efekty naszej współpracy przerosły moje oczekiwania.
-      W ciągu 3 miesięcy liczba klientów wzrosła o 40%, a moja marka
-      zyskała wyrazisty charakter i rozpoznawalność wśród odbiorców.`,
+    text: `Efekty naszej współpracy przerosły moje oczekiwania.
+    W ciągu 3 miesięcy liczba klientów wzrosła o 40%, a moja marka
+    zyskała wyrazisty charakter i rozpoznawalność wśród odbiorców.`,
     author: "Karolina Wiśniewska",
     role: `Gabinet Naturalnej Terapii "Natura"`,
   },
 ];
 
-const adjustedTestimonials = [...testimonials, testimonials[0]];
+const adjustedTestimonials = [
+  testimonials[testimonials.length - 1],
+  ...testimonials,
+  testimonials[0],
+];
 
 export const Testimonials = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [visibleIndex, setVisibleIndex] = useState(1);
+  const [isTransitioning, setIsTransitioning] = useState(true);
+  const [isTransitionComplete, setIsTransitionComplete] = useState(true);
+  const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
+
+  const startSlider = () => {
+    if (intervalRef.current) clearInterval(intervalRef.current);
+
+    intervalRef.current = setInterval(() => {
+      handleNext();
+    }, 5000);
+  };
 
   useEffect(() => {
-    if (activeIndex !== adjustedTestimonials.length - 1) return;
+    startSlider();
+    return () => clearInterval(intervalRef.current!);
+  }, []);
 
-    const timeout = setTimeout(() => {
-      setActiveIndex(0);
-    }, 700);
+  useEffect(() => {
+    if (visibleIndex === adjustedTestimonials.length - 1) {
+      const timeout = setTimeout(() => {
+        setIsTransitioning(false);
+        setVisibleIndex(1);
+      }, 600);
 
-    return () => clearTimeout(timeout);
-  }, [activeIndex]);
+      return () => clearTimeout(timeout);
+    } else {
+      setIsTransitioning(true);
+    }
 
-  const prevSlide = () => {
-    setActiveIndex((prev) => (prev === 0 ? testimonials.length - 1 : prev - 1));
+    if (visibleIndex === 0) {
+      const timeout = setTimeout(() => {
+        setIsTransitioning(false);
+        setVisibleIndex(adjustedTestimonials.length - 2);
+      }, 600);
+
+      return () => clearTimeout(timeout);
+    } else {
+      setIsTransitioning(true);
+    }
+  }, [visibleIndex]);
+
+  const handleNext = () => {
+    if (isTransitionComplete) {
+      setIsTransitionComplete(false);
+      setVisibleIndex((prev) => prev + 1);
+      startSlider();
+      setTimeout(() => {
+        setIsTransitionComplete(true);
+      }, 800);
+    } else {
+      return;
+    }
+    console.log(visibleIndex);
+    console.log(realIndex);
   };
 
-  const nextSlide = () => {
-    setActiveIndex((prev) => (prev === testimonials.length - 1 ? 0 : prev + 1));
+  const handlePrev = () => {
+    if (isTransitionComplete) {
+      setIsTransitionComplete(false);
+      setVisibleIndex((prev) => prev - 1);
+      startSlider();
+      setTimeout(() => {
+        setIsTransitionComplete(true);
+      }, 800);
+    } else {
+      return;
+    }
+
+    console.log(visibleIndex);
+    console.log(realIndex);
   };
+
+  const goTo = (index: number) => {
+    setVisibleIndex(index + 1);
+    setIsTransitioning(true);
+    startSlider();
+  };
+
+  const realIndex = (() => {
+    if (visibleIndex === 0) return testimonials.length - 1;
+    if (visibleIndex === adjustedTestimonials.length - 1) return 0;
+    return visibleIndex - 1;
+  })();
 
   return (
     <section
@@ -61,7 +127,7 @@ export const Testimonials = () => {
       className={cn(styles.testimonials, styles.section)}
     >
       <div className="container">
-        <div className="section-title" data-aos="fade-up">
+        <div className="section-title">
           <h2>Co mówią nasi klienci</h2>
           <p>
             Zobacz, jak współpraca z nami wpłynęła na rozwój biznesów naszych
@@ -69,23 +135,16 @@ export const Testimonials = () => {
           </p>
         </div>
 
-        <div
-          className={cn(styles["testimonials-slider"], {
-            [styles["no-transition"]]: activeIndex === 0,
-          })}
-          data-aos="fade-up"
-          data-aos-delay={300}
-        >
+        <div className={styles["testimonials-slider"]}>
           <div
             className={cn(styles["testimonials-track"], {
-              [styles["no-transition"]]: activeIndex === 0,
+              [styles["no-transition"]]: !isTransitioning,
             })}
-            style={{ transform: `translateX(-${activeIndex * 100}%)` }}
-            id="testimonialsSlider"
+            style={{ transform: `translateX(-${visibleIndex * 100}%)` }}
           >
-            {adjustedTestimonials.map((item) => (
+            {adjustedTestimonials.map((item, i) => (
               <div
-                key={`testimonial+${item.id}`}
+                key={`testimonial+${item.id}+${i}`}
                 className={styles["testimonial-slide"]}
               >
                 <div className={styles["testimonial-content"]}>
@@ -123,48 +182,30 @@ export const Testimonials = () => {
           <div className={styles["testimonial-controls"]}>
             <button
               className={cn(styles["testimonial-arrow"], styles.prev)}
-              id="prevTestimonial"
-              onClick={prevSlide}
+              onClick={handlePrev}
             >
               <i className="fas fa-chevron-left"></i>
             </button>
-            <div className={styles["testimonial-dots"]} id="testimonialDots">
+            <div className={styles["testimonial-dots"]}>
               {testimonials.map((_, index) => (
                 <span
                   key={`dot+${index}`}
                   className={cn(
                     styles.dot,
-                    index === activeIndex && styles.active
+                    index === realIndex && styles.active
                   )}
-                  onClick={() => setActiveIndex(index)}
+                  onClick={() => goTo(index)}
                 ></span>
               ))}
             </div>
             <button
               className={cn(styles["testimonial-arrow"], styles.next)}
-              id="nextTestimonial"
-              onClick={nextSlide}
+              onClick={handleNext}
             >
               <i className="fas fa-chevron-right"></i>
             </button>
           </div>
         </div>
-
-        {/* <div
-          className={styles["video-testimonial"]}
-          data-aos="fade-up"
-          data-aos-delay="300"
-        >
-          <h3>Zobacz videoopinię naszego klienta</h3>
-          <div className={styles["video-container"]}>
-            <div className={styles["video-placeholder"]}>
-              <img src="/api/placeholder/800/450" alt="Video Testimonial" />
-              <div className={styles["play-button"]}>
-                <i className="fas fa-play"></i>
-              </div>
-            </div>
-          </div>
-        </div> */}
       </div>
     </section>
   );
